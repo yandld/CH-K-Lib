f25a1bb0d05f518cb5fe6002e91eac5da1a7ae94
yandld <yandld@126.com>
2014-04-06 11:39:42 +0800

    refine

--- Project/超核K60开发板-渡鸦/RTOS/src/gui_thread.c
+++ Project/超核K60开发板-渡鸦/RTOS/src/gui_thread.c
@@ -4,6 +4,9 @@
 #include "DIALOG.h"
 
 
+#define     TOUCH_IDLE          (0x00)
+#define     TOUCH_STATUS2       (0x01)
+
 static void GUI_SettingInit(void)
 {
 	GUI_SetBkColor(GUI_BLACK); 	
@@ -17,28 +20,62 @@ static void GUI_SettingInit(void)
 void gui_thread_entry(void* parameter)
 {
     rt_thread_t thread = rt_thread_self();
+    static rt_uint32_t Status;
     GUI_Init();//³õÊ¼»¯GUI 
     GUI_SettingInit();
 	GUI_DispString("GUI system OK");//ÏÔÊ¾²âÊÔ
+    WM_HWIN hWin;
    // CreateWelcome();
   //  GUI_Exec();
   //  show_pic("/SD/PIC/INDEX.BMP");
   //  GUI_CURSOR_Show();
     
-    GUIDEMO_IconView();
+    hWin = GUI_Desktop();
    // MainMenu_Init();
   //  while(WM_GetWindowOrgY(hWin) > 0)
     {
     //    WM_MoveWindow(hWin, 0, -6);
    //     rt_thread_delay(1);
     }
-  //  MainTask();
+    
+    GUI_PID_STATE  InitalTouchPoint;
+    GUI_PID_STATE  TouchPoint;
 	while(1)
 	{
+        GUI_TOUCH_GetState(&TouchPoint);
+        switch(Status)
+        {
+            case TOUCH_IDLE:
+                if(TouchPoint.Pressed)
+                {
+                    InitalTouchPoint = TouchPoint;
+                    Status = TOUCH_STATUS2;
+                }
+                break;
+            case TOUCH_STATUS2:
+                if(TouchPoint.Pressed)
+                {
+                    if(abs(InitalTouchPoint.x - TouchPoint.x) > 5)
+                    {
+                        WM_MoveWindow(hWin, TouchPoint.x - InitalTouchPoint.x , 0);
+                    }
+                    Status = TOUCH_STATUS2;
+                }
+                else
+                {
+                    Status = TOUCH_IDLE;
+                }
+                break;
+        }
         
-     //   WM_MoveCtrl();
+        if(TouchPoint.Pressed)
+        {
+          //  WM_MoveWindow(hWin, 1, 0);
+        }
         
-       // GUI_TOUCH_Exec();
-        rt_thread_delay(10);
+        //   WM_MoveCtrl();
+        
+        // GUI_TOUCH_Exec();
+        rt_thread_delay(1);
 	}
 }
--- Project/超核K60开发板-渡鸦/RTOS/src/led_thread.c
+++ Project/超核K60开发板-渡鸦/RTOS/src/led_thread.c
@@ -8,26 +8,10 @@
 
 void led_thread_entry(void* parameter)
 {
-    GUI_PID_STATE  TouchPoint;
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
 	while(1)
 	{
         GUI_TOUCH_Exec();
-        		GUI_TOUCH_GetState(&TouchPoint);
-        		if (TouchPoint.Pressed)
-		{
-			if(TouchFlag == TouchUnPressed)
-			{
-				TouchFlag = TouchPress;
-			}
-		}
-		else
-		{
-			if(TouchFlag == TouchPressed)
-			{
-				TouchFlag = TouchUnPress;	
-			}
-		}
         GUI_Exec();
         GPIO_ToggleBit(HW_GPIOE, 6);
         rt_thread_delay(2);

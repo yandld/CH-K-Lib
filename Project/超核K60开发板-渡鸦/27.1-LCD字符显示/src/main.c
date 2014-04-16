304007359978c49a2a32527caa351101cd43d534
yandld <yandld@126.com>
2014-04-16 15:47:06 +0800

    Minor refine

--- Project/超核K60开发板-渡鸦/27.1-LCD字符显示/src/main.c
+++ Project/超核K60开发板-渡鸦/27.1-LCD字符显示/src/main.c
@@ -1,16 +1,13 @@
 #include "gpio.h"
 #include "common.h"
 #include "uart.h"
-#include "flexbus.h"
-
-#include "sram.h"
 #include "ili9320.h"
 
 #include "gui.h"
 
 int main(void)
 {
-    uint8_t i;
+    uint32_t i;
     DelayInit();
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
@@ -19,8 +16,15 @@ int main(void)
     GUI_Init();
     GUI_DispString("ucGUI");
     GUI_DispString(GUI_GetVersionString());
-    while(1);
-
+    GUI_DispString("\r\nHello world!");
+    while(1) 
+    {
+        GUI_DispDecAt( i++, 20,20,4);
+        if (i>9999)
+        {
+            i=0;
+        }
+    }
 }
 
 

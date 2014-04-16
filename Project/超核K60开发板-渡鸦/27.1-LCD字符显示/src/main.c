2ed99a2ecfd81e378a6c32898a50fadfc0f17dda
yandld <yandld@126.com>
2014-04-16 13:22:11 +0800

    Refine

--- Project/超核K60开发板-渡鸦/27.1-LCD字符显示/src/main.c
+++ Project/超核K60开发板-渡鸦/27.1-LCD字符显示/src/main.c
@@ -7,16 +7,6 @@
 #include "ili9320.h"
 
 #include "gui.h"
-static uint16_t ColorTable[] = 
-{
-    RED,
-    BLUE,
-    YELLOW,
-    GREEN,
-    CYAN,
-    LGRAY,
-    GRED,
-};
 
 int main(void)
 {
@@ -26,19 +16,11 @@ int main(void)
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
     
     printf("flexbus lcd test\r\n");
-    ili9320_init();
-    printf("controller id:0x%X\r\n", ili9320_get_id());
     GUI_Init();
-    GUI_SetFont(&GUI_Font8x16);
-    GUI_DispStringAt("hello",0,0);
+    GUI_DispString("ucGUI");
+    GUI_DispString(GUI_GetVersionString());
     while(1);
-    while(1)
-    {
-        i++; i%= ARRAY_SIZE(ColorTable);
-        ili9320_clear(ColorTable[i]);
-        GPIO_ToggleBit(HW_GPIOE, 6);
-        DelayMs(500);
-    }
+
 }
 
 

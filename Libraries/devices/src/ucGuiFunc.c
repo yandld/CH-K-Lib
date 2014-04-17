3a2fc466462bcf850dc532af197839aead39cc9b
yandld <yandld@126.com>
2014-04-17 20:20:55 +0800

    Refine touch driver

--- Libraries/devices/src/ucGuiFunc.c
+++ Libraries/devices/src/ucGuiFunc.c
@@ -1,20 +1,38 @@
 #include "ili9320.h"
 #include "ads7843.h"
-
+#include "common.h"
 
 /* 重定义 GUI接口函数 */
 int GUI_TOUCH_X_MeasureX(void)
 {
+    int ret;
     int x;
+    static int x_last;
     ads7843_readX(&x);
-    return x;
+    ret = x;
+    if(ABS(x - x_last) > 5)
+    {
+        /* discard this result */
+        ret = x_last;
+    }
+    x_last = x;
+    return ret;
 }
 
 int GUI_TOUCH_X_MeasureY(void)
 {
-    int y;
-    ads7843_readY(&y);
-    return y;
+    int ret;
+    int x;
+    static int x_last;
+    ads7843_readY(&x);
+    ret = x;
+    if(ABS(x - x_last) > 5)
+    {
+        /* discard this result */
+        ret = x_last;
+    }
+    x_last = x;
+    return ret;
 }
 
 int LCD_L0_Init(void)

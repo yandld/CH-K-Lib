f2c0b79af7ea21a965d205591b9428880b2aff90
zhangxing <zhang666xing@163.com>
2014-05-03 13:14:45 +0800

    add landz  to gpio main

--- Project/蓝宙K60核心板/0-工程模板/src/main.c
+++ Project/蓝宙K60核心板/0-工程模板/src/main.c
@@ -1,46 +1,51 @@
 #include "gpio.h"
 #include "common.h"
 #include "uart.h"
-
 /* CH Kinetis固件库 V2.50 版本 */
-/* 修改主频 请使用 CMSIS标准文件 system_MKxxxx.c 中的 CLOCK_SETUP 宏 */
+/* 修改主频 请修改 CMSIS标准文件 system_MKxxxx.c 中的 CLOCK_SETUP 宏 */
 
-/* UART 快速初始化结构所支持的引脚* 使用时还是推荐标准初始化 */
+/* GPIO 工作模式 */
 /*
- UART1_RX_PE01_TX_PE00   
- UART0_RX_PF17_TX_PF18   
- UART3_RX_PE05_TX_PE04   
- UART5_RX_PF19_TX_PF20   
- UART5_RX_PE09_TX_PE08   
- UART2_RX_PE17_TX_PE16   
- UART4_RX_PE25_TX_PE24   
- UART0_RX_PA01_TX_PA02   
- UART0_RX_PA15_TX_PA14   
- UART3_RX_PB10_TX_PB11   
- UART0_RX_PB16_TX_PB17   
- UART1_RX_PC03_TX_PC04   
- UART4_RX_PC14_TX_PC15   
- UART3_RX_PC16_TX_PC17   
- UART2_RX_PD02_TX_PD03   
- UART0_RX_PD06_TX_PD07   
- UART2_RX_PF13_TX_PF14   
- UART5_RX_PD08_TX_PD09   
+ *         @arg kGPIO_Mode_IFT :悬空输入
+ *         @arg kGPIO_Mode_IPD :下拉输入
+ *         @arg kGPIO_Mode_IPU :上拉输入
+ *         @arg kGPIO_Mode_OOD :开漏输出 如果不清楚这2种输出的区别请 百度..
+ *         @arg kGPIO_Mode_OPP :推挽输出
+ */
+ 
+/*
+     实验名称：GPIO小灯实验
+     实验平台：蓝宙K60核心板
+     板载芯片：MK60DN512ZVQ10
+     说    明：本实例程序及固件库由超核电子提供
+     程序更新：www.beyondcore.net
+     实验效果：控制PTA端口的17引脚是输出模式
+               在低电平时小灯亮起，在高电平时小灯熄灭
+               小灯周期性闪烁，闪烁时间间隔500ms     
 */
  
 int main(void)
 {
+    /* 初始化Delay */
     DelayInit();
-    /* 初始化一个引脚 设置为推挽输出 */
-    GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
-    /* 初始化一个串口 使用UART0端口的PTD6引脚和PTD7引脚作为接收和发送，默认设置 baud 115200 */
-    UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
+    /* 使用简易初始化初始化PTA端口的17引脚作为IO使用 设置为推挽输出模式 */
+    GPIO_QuickInit(HW_GPIOA, 17, kGPIO_Mode_OPP);
+    /* 另外一种初始化方式: 结构体模式的初始化 有点类似STM32固件库*/
+//    GPIO_InitTypeDef GPIO_InitStruct1;
+//    GPIO_InitStruct1.instance = HW_GPIOA;
+//    GPIO_InitStruct1.mode = kGPIO_Mode_OPP;
+//    GPIO_InitStruct1.pinx = 17;
+//    GPIO_Init(&GPIO_InitStruct1);
     
-    printf("HelloWorld!\r\n");
+    /* 控制PTA端口的17引脚输出低电平 */
+    GPIO_WriteBit(HW_GPIOA, 17, 0);
     
     while(1)
     {
-        /* 闪烁小灯 */
-        GPIO_ToggleBit(HW_GPIOE, 6);
+//        /* 翻转引脚电平 原来是低变成高 原来是高变成低 */
+//        GPIO_ToggleBit(HW_GPIOA, 17);
+        /* 另外一种方法 使用位带操作 */
+        PAout(17) = !PAout(17);
         DelayMs(500);
     }
 }

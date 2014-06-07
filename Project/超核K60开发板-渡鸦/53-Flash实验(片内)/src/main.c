0cf4d420c34174d33b480e8466acafc41d2dbb6a
Yandld <yandld@126.com>
2014-06-07 17:48:25 +0800

    Refine demos

--- Project/超核K60开发板-渡鸦/53-Flash实验(片内)/src/main.c
+++ Project/超核K60开发板-渡鸦/53-Flash实验(片内)/src/main.c
@@ -1,6 +1,7 @@
 #include "gpio.h"
 #include "common.h"
 #include "uart.h"
+#include "pit.h"
 #include "FlashOS.H"
 
 /* 
@@ -16,19 +17,22 @@ static int MKP512FlashInit(void)
     return Init(0x00000000, clock, 2);
 }
 
+#define FLASH_SIZE      0x80000
 #define SECTER_SIZE     0x000800
 extern int Image$$ER_IROM1$$Limit;
-#define TEST_ADDR_BEIGN  (uint32_t)(&Image$$ER_IROM1$$Limit + SECTER_SIZE)
+#define TEST_ADDR_BEIGN  (uint32_t)(FLASH_SIZE - SECTER_SIZE)
 
 int main(void)
 {
     static uint8_t buf[SECTER_SIZE];
     uint32_t i;
+    uint32_t ticks;
     uint32_t r;
     DelayInit();
+    
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
-    
+    PIT_QuickInit(HW_PIT_CH0, 1000*1000);
     printf("init standard flash driver...\r\n");
     r = MKP512FlashInit();
     if(r)
@@ -44,8 +48,18 @@ int main(void)
     }
 
     /* ersase and program */
+    PIT_ResetCounter(HW_PIT_CH0);
+    ticks = PIT_GetCounterValue(HW_PIT_CH0);
     EraseSector(TEST_ADDR_BEIGN);
+    ticks = ticks - PIT_GetCounterValue(HW_PIT_CH0);
+    printf("EraseSector takes %d ticks\r\n", ticks);
+    
+    PIT_ResetCounter(HW_PIT_CH0);
+    ticks = PIT_GetCounterValue(HW_PIT_CH0);
     r = ProgramPage(TEST_ADDR_BEIGN, sizeof(buf), buf);
+    ticks = ticks - PIT_GetCounterValue(HW_PIT_CH0);
+    printf("ProgramPage takes %d ticks\r\n", ticks);
+    
     if(r)
     {
         printf("program page failed\r\n");
@@ -55,9 +69,12 @@ int main(void)
     uint8_t *p = (uint8_t*)TEST_ADDR_BEIGN;
     for(i=0;i<sizeof(buf);i++)
     {
-        printf("0x%02X ", *p++);
+        if(*p++ != (i%0xFF))
+        {
+            printf("error:0x%02X ", *p++);
+        }
     }
-    
+    printf("flash test completed!\r\n");
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOE, 6);

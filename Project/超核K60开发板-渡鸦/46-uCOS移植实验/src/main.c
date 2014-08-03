befee464db87310c854fd812df8124c36fbf0438
Yandld <yandld@126.com>
2014-08-03 14:44:52 +0800

    Add uCOSII demo

---
+++ Project/超核K60开发板-渡鸦/46-uCOS移植实验/src/main.c
@@ -0,0 +1,62 @@
+#include "gpio.h"
+#include "common.h"
+#include "systick.h"
+#include "uart.h"
+//uCOS
+#include "includes.h"
+
+
+
+//任务堆栈大小
+#define TASK_STK_SIZE              (128)
+//定义任务优先级
+#define APP_LED1_TASK_PRIO         (5)
+#define APP_LED0_TASK_PRIO         (7)
+//声明任务堆栈
+OS_STK  APP_LED1_STK[TASK_STK_SIZE];
+OS_STK  APP_LED0_STK[TASK_STK_SIZE];
+//LED小灯任务
+void AppLED1Task(void *pdata)
+{
+  pdata = pdata; //防止编译器出错 无实际意义
+	while(1)
+	{
+		printf("AppLED 1 Task:Process\r\n");
+		OSTimeDlyHMSM(0, 0, 0, 500);
+	}
+}
+
+void AppLED0Task(void *pdata)
+{
+  pdata = pdata; //防止编译器出错 无实际意义
+	while(1)
+	{
+		printf("AppLED 2 Task:Process\r\n");
+		OSTimeDlyHMSM(0, 0, 0, 500);
+	}
+}
+
+
+int main(void)
+{
+
+    DelayInit();
+    SYSTICK_ITConfig(true);
+    
+    UART_QuickInit(UART5_RX_PE09_TX_PE08, 115200);
+    
+    printf("uCOSII test\r\n");
+
+    OSInit();  //OS初始化
+    OSTaskCreate(AppLED1Task,(void *)0,
+                &APP_LED1_STK[TASK_STK_SIZE-1],
+                APP_LED1_TASK_PRIO); //建立LED1任务
+    OSTaskCreate(AppLED0Task,(void *)0,
+                &APP_LED0_STK[TASK_STK_SIZE-1],
+                APP_LED0_TASK_PRIO); //建立LED0任务
+    OSStart(); //控制权交给操作系统
+	//程序永远不会运行到这
+    while(1);
+}
+
+

4121b67c9c2072ed003b54d6abf6b93560a8647a
Rein <yixiao.liu@live.cn>
2014-11-18 23:25:22 +0800

    Insert New DSP Demo

---
+++ Project/超核K60开发板-渡鸦/70-DSP数学库入门/src/dsp_demo.c
@@ -0,0 +1,15 @@
+#include "chlib_k.h"
+#include "arm_math.h"
+#define PA  (3.1415926f)
+
+
+ /**
+ * @brief  PIT模块快速初始化配置
+ * @code
+ * @endcode
+ * @param  chl 通道号
+ * @param  timeInUs 产生中断的周期 \单位US
+ * @retval None
+ */
+void arm_abs_f32_demo(void);
+

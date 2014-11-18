054138d28b6e5f2bc1ae998799035d54909e2ac5
yandld <yandld>
2014-11-18 23:02:27 +0800

    Demo DSP Refine -LYX

--- Project/超核K60开发板-渡鸦/70-DSP数学库入门/src/main.c
+++ Project/超核K60开发板-渡鸦/70-DSP数学库入门/src/main.c
@@ -8,13 +8,6 @@ int main(void)
     DelayInit();
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
-
-    float32_t in = (PA*PA);
-    float32_t out;
-    /* 做一个开根号测试 */
-    arm_sqrt_f32(in, &out);
-    printf("arm_sqrt_f32:sqrt(%f) = %f\r\n",in, out);
-    
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOE, 6);

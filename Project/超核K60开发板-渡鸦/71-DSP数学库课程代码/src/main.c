d756d713bc0dcca843a69cc9ad37d2d54661d928
Rein <yixiao.liu@live.cn>
2014-11-22 00:03:49 +0800

    Refine Demo 71 add a simple demo in main();

--- Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/main.c
+++ Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/main.c
@@ -5,6 +5,7 @@ int main(void)
     DelayInit();
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
+	arm_abs_f32_demo();
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOE, 6);

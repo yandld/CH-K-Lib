f2c95e9c462e7f5f5cbe35b3f82ec0496715f073
Rein <yixiao.liu@live.cn>
2014-11-22 19:49:03 +0800

    Refine demo 71 add 7s examples.

--- Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/main.c
+++ Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/main.c
@@ -5,7 +5,15 @@ int main(void)
     DelayInit();
     GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
-	arm_abs_f32_demo();
+//	arm_abs_f32_demo();			//abs demo
+//	arm_add_f32_demo();			//add demo
+//	arm_dot_prod_f32_demo();	//dot prod demo
+//	arm_mult_f32_demo();		//mult demo
+//	arm_negate_f32_demo();		//negate demo
+//	arm_offset_f32_demo();		//offset demo
+//	arm_scale_f32_demo();		//scale demo
+	arm_sub_f32_demo();
+
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOE, 6);

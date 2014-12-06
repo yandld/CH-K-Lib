c95ff730f5216dc23828bca1bb9d4e3e77e4f4d6
Rein <yixiao.liu@live.cn>
2014-12-06 23:29:02 +0800

    Refine demo-71

--- Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/demo/dsp_complex_demo.c
+++ Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/demo/dsp_complex_demo.c
@@ -37,7 +37,7 @@ void arm_cmplx_conj_f32_demo (void) {
  * @param  None
  * @retval None
  * @author Rein
- * @update 2014-12-2
+ * @update 2014-12-6
  */
 
 void arm_cmplx_dot_prod_f32_demo (void) {
@@ -63,7 +63,7 @@ void arm_cmplx_dot_prod_f32_demo (void) {
  * @param  None
  * @retval None
  * @author Rein
- * @update 2014-12-2
+ * @update 2014-12-6
  */
 
 void arm_cmplx_mag_f32_demo (void) {
@@ -84,3 +84,57 @@ void arm_cmplx_mag_f32_demo (void) {
 	printf("Real of Complex_3 is %f\n\r",result[2]);
 }
  
+ /**
+ * @brief  演示arm_cmplx_mag_squared_f32功能
+ * @code
+ * @endcode
+ * @param  None
+ * @retval None
+ * @author Rein
+ * @update 2014-12-6
+ */
+void arm_cmplx_mag_squared_f32_demo (void) {
+	float32_t Complex[6] = 	{
+								10.5f,10.6f,		/**前面是实部，后面是虚部，下同*/
+								11.2f,56.3f,
+								25.5f,7.2f
+							};
+	float32_t result[3] = {0,0,0};			/**保存运算结果*/
+	
+	arm_cmplx_mag_f32(Complex,result,3);	/**运算*/
+	
+	printf("Complex_1 = %f + %fj\n\r",Complex[0],Complex[1]);
+	printf("Complex_2 = %f + %fj\n\r",Complex[2],Complex[3]);
+	printf("Complex_3 = %f + %fj\n\r",Complex[4],Complex[5]);	
+	printf("Real of Complex_1 is %f\n\r",result[0]);
+	printf("Real of Complex_2 is %f\n\r",result[1]);
+	printf("Real of Complex_3 is %f\n\r",result[2]);
+}
+ /**
+ * @brief  演示arm_cmplx_mult_cmplx_f32功能
+ * @code
+ * @endcode
+ * @param  None
+ * @retval None
+ * @author Rein
+ * @update 2014-12-6
+ */
+void arm_cmplx_mult_cmplx_f32_demo (void) {
+	float32_t A[4] = 	{
+							0.5f,12.3f,
+							56.3f,10.0f
+						};
+	float32_t B[4] = 	{
+							1.5f,28.6f,
+							52.83f,25.0f
+						};
+	float32_t result[4] ={0};
+	uint32_t number_of_complex = 2;
+	
+	arm_cmplx_mult_cmplx_f32(A,B,result,number_of_complex);
+	/**打印准备计算的复数*/
+	printf("A0 = (%f,%fj)  A1 = (%f,%fj)\n\r",A[0],A[1],A[2],A[3]);
+	printf("B0 = (%f,%fj)  B1 = (%f,%fj)\n\r",B[0],B[1],B[2],B[3]);
+	/**打印计算结果*/
+	printf("result0 = (%f,%fj)  result1 = (%f,%fj)\n\r",result[0],result[1],result[2],result[3]);
+}
--- Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/demo/dsp_complex_demo.h
+++ Project/超核K60开发板-渡鸦/71-DSP数学库课程代码/src/demo/dsp_complex_demo.h
@@ -6,5 +6,7 @@
 void arm_cmplx_conj_f32_demo (void);
 void arm_cmplx_dot_prod_f32_demo (void); 
 void arm_cmplx_mag_f32_demo (void);
+void arm_cmplx_mag_squared_f32_demo (void);
+void arm_cmplx_mult_cmplx_f32_demo (void);
 #else
 #endif

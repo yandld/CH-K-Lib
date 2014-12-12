b22c4f1f74da73135b344ea7991ff8eac9556e38
YangXi <B51439@freescale.com>
2014-12-12 15:27:03 +0800

    Refine K64 templete

--- Project/超核K64开发板-渡鸦/0-工程模板/src/main.c
+++ Project/超核K64开发板-渡鸦/0-工程模板/src/main.c
@@ -32,7 +32,7 @@ int main(void)
 {
     DelayInit();
     /* 初始化一个引脚 设置为推挽输出 */
-    GPIO_QuickInit(HW_GPIOB, 21, kGPIO_Mode_OPP);
+    GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
     /* 初始化一个串口 使用UART0端口的PTD6引脚和PTD7引脚作为接收和发送，默认设置 baud 115200 */
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
     
@@ -40,7 +40,7 @@ int main(void)
     while(1)
     {
         /* 闪烁小灯 */
-        GPIO_ToggleBit(HW_GPIOB, 21);
+        GPIO_ToggleBit(HW_GPIOE, 6);
         DelayMs(500);
     }
 }

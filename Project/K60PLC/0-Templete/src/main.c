bd542dbad058ee2e07b8a06b5e767fa66b49879e
Rein <yixiao.liu@live.cn>
2014-12-27 16:33:17 +0800

    Refresh plc-0

--- Project/K60PLC/0-Templete/src/main.c
+++ Project/K60PLC/0-Templete/src/main.c
@@ -1,37 +1,45 @@
-
 #include "chlib_k.h"
 #include "dsp_basic_demo.h"
 #include "dsp_fast_demo.h"
 #include "dsp_complex_demo.h"
+#include "main.h"
+
+uint32_t fftSize = 32; 
+uint32_t ifftFlag = 0; 
+uint32_t doBitReverse = 1;
+int ix;
 int main(void)
 {
+	SYSTICK_Init(1000*1000);
+    SYSTICK_Cmd(true);
+   
     DelayInit();
     GPIO_QuickInit(HW_GPIOE, 8, kGPIO_Mode_OPP);
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
     printf("System start!\r\n");
-    
-	/**It's clear to display choose 1 demo*/
-//	/**basic fun*/
-//	arm_abs_f32_demo();			
-//	arm_add_f32_demo();			
-//	arm_dot_prod_f32_demo();	
-//	arm_mult_f32_demo();		
-//	arm_negate_f32_demo();		
-//	arm_offset_f32_demo();		
-//	arm_scale_f32_demo();		
-//	arm_sub_f32_demo();
+	
+    /**实数信号转换成复数信号*/
+	for(int i=0;i<31;i++) {
+	 test_input[2*i] = test_signal[i];
+	 test_input[2*i+1] = 0;		
+	}
+	/**转换后的输出测试*/
+//	printf("y = [");
+//	for(int i=0;i<64;i++) {
+//		printf("%f ",test_input[i]);
+//	}
+//	printf("]\n\r");
+	 int  xx = SYSTICK_GetVal();
 
-//	/**fast fun*/
-//	arm_cos_f32_demo ();
-//	arm_sin_f32_demo ();
-//	arm_sqrt_f32_demo();
-//	
-//	/**complex fun*/
-//	arm_cmplx_conj_f32_demo();
-//	arm_cmplx_dot_prod_f32_demo (); 
-//	arm_cmplx_mag_f32_demo ();
-//	arm_cmplx_mag_squared_f32_demo();
-//	arm_cmplx_mult_cmplx_f32_demo();
+	arm_cfft_f32(&arm_cfft_sR_f32_len32, test_input, ifftFlag, doBitReverse);
+	arm_cmplx_mag_f32(test_input, test_output, fftSize);
+    printf("ADC_QuickReadValue cost: %dticks\r\n", xx - SYSTICK_GetVal());
+
+	printf("y = [");
+	for(int i=0;i<32;i++) {
+		printf("%f ",test_output[i]);
+	}
+	printf("];\n\r");
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOE, 8);

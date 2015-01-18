db8bf4dd91b35c086d4862564c8edf9924b92cd9
yandld <yandld@126.com>
2015-01-18 20:15:07 +0800

    Refine RTOS

--- Project/超核K60开发板-渡鸦/RTOS/src/gui/dialog/desktop/gui_desktop.c
+++ Project/超核K60开发板-渡鸦/RTOS/src/gui/dialog/desktop/gui_desktop.c
@@ -2,15 +2,15 @@
 
 
 #define ID_WINDOW_0     (GUI_ID_USER + 0x01)
-#define ID_FRAMEWIN_0  (GUI_ID_USER + 0x00)
-#define ID_ICONVIEW_0 (GUI_ID_USER + 0x01)
-#define ID_BUTTON_0 (GUI_ID_USER + 0x01)
-#define ID_BUTTON_1 (GUI_ID_USER + 0x02)
-#define ID_BUTTON_2 (GUI_ID_USER + 0x03)
-#define ID_BUTTON_3 (GUI_ID_USER + 0x04)
-#define ID_BUTTON_4 (GUI_ID_USER + 0x05)
-#define ID_BUTTON_5 (GUI_ID_USER + 0x06)
-#define ID_IMAGE_0          (GUI_ID_USER + 0x04)
+#define ID_FRAMEWIN_0   (GUI_ID_USER + 0x00)
+#define ID_ICONVIEW_0   (GUI_ID_USER + 0x01)
+#define ID_BUTTON_0     (GUI_ID_USER + 0x01)
+#define ID_BUTTON_1     (GUI_ID_USER + 0x02)
+#define ID_BUTTON_2     (GUI_ID_USER + 0x03)
+#define ID_BUTTON_3     (GUI_ID_USER + 0x04)
+#define ID_BUTTON_4     (GUI_ID_USER + 0x05)
+#define ID_BUTTON_5     (GUI_ID_USER + 0x06)
+#define ID_IMAGE_0      (GUI_ID_USER + 0x04)
 
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate2[] = {
@@ -19,7 +19,7 @@ static const GUI_WIDGET_CREATE_INFO _aDialogCreate2[] = {
     
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate[] = {
     { WINDOW_CreateIndirect, "Window", ID_WINDOW_0, 0, 0, 320, 240, 0, 0x0, 0 },
-  //  { IMAGE_CreateIndirect, "Image", ID_IMAGE_0, 0, 0, 240, 320, 0, IMAGE_CF_MEMDEV | IMAGE_CF_ATTACHED | IMAGE_CF_TILE | IMAGE_CF_ALPHA, 0 },
+    { IMAGE_CreateIndirect, "Image", ID_IMAGE_0, 0, 0, 240, 320, 0, IMAGE_CF_MEMDEV | IMAGE_CF_ATTACHED | IMAGE_CF_TILE | IMAGE_CF_ALPHA, 0 },
     { BUTTON_CreateIndirect, "Cal",           ID_BUTTON_0, 80, 10, 50, 50, 0, 0x0, 0 },
     { BUTTON_CreateIndirect, "Info",          ID_BUTTON_1, 0, 10, 50, 50, 0, 0x0, 0 },
     { BUTTON_CreateIndirect, "Time",          ID_BUTTON_2, 0, 80, 50, 50, 0, 0x0, 0 },
@@ -27,27 +27,6 @@ static const GUI_WIDGET_CREATE_INFO _aDialogCreate[] = {
     { BUTTON_CreateIndirect, "Cali",          ID_BUTTON_4, 71, 49, 50, 50, 0, 0x0, 0 },
  // { BUTTON_CreateIndirect, "Test",          ID_BUTTON_5, 137, 49, 50, 50, 0, 0x0, 0 },
 };
-
-
-static void _cb(WM_MESSAGE * pMsg)
-{
-    WM_HWIN hItem;
-    switch (pMsg->MsgId)
-    {
-        case WM_INIT_DIALOG:
-        hItem = pMsg->hWin;
-        FRAMEWIN_SetBorderSize(hItem, 0);
-        FRAMEWIN_SetTitleVis(hItem, 0);
-        FRAMEWIN_SetClientColor(hItem, GUI_WHITE);
-        break;
-        case WM_PAINT:
-        
-        break;
-        default:
-        WM_DefaultProc(pMsg);
-        break;
-    }        
-}
 
 
 
@@ -59,7 +38,7 @@ static void _cbDialog2(WM_MESSAGE * pMsg) {
         hItem = pMsg->hWin;
         FRAMEWIN_SetBorderSize(hItem, 0);
         FRAMEWIN_SetTitleVis(hItem, 0);
-        FRAMEWIN_SetClientColor(hItem, GUI_WHITE);
+        FRAMEWIN_SetClientColor(hItem, GUI_LIGHTBLUE);
         break;
         case WM_PAINT:
         
@@ -73,10 +52,10 @@ static void _cbDialog2(WM_MESSAGE * pMsg) {
 
 static char *AppImageTable[] = 
 {
-    "/SD/GAME.BMP",
-    "/SD/WIFI.BMP",
-    "/SD/FILE.BMP",
-    "/SD/PER.BMP",
+    "/SF/SYS/GAME.BMP",
+    "/SF/SYS/WIFI.BMP",
+    "/SF/SYS/FILE.BMP",
+    "/SF/SYS/PER.BMP",
 };
 
 static void _cbDialog(WM_MESSAGE * pMsg) {
@@ -95,46 +74,36 @@ static void _cbDialog(WM_MESSAGE * pMsg) {
         WM_EnableMemdev(hItem);
         
         WM_MOTION_SetMoveable(hItem, WM_CF_MOTION_X, 1);
-       // FRAMEWIN_SetMoveable(pMsg->hWin, 1);
-        WINDOW_SetBkColor(hItem, GUI_WHITE);
+        WINDOW_SetBkColor(hItem, GUI_LIGHTBLUE);
         int fd; rt_uint8_t *p[6];
         
-        p[2] = rt_malloc(38399);
-        fd = open("/SD/DESKTOP.JPG",O_RDONLY , 0);
-        read(fd, p[2], 38399);
-        hItem = WM_GetDialogItem(pMsg->hWin, ID_IMAGE_0);
-        WM_EnableMemdev(hItem);
-     //     IMAGE_SetJPEG(hItem, p[2], 38399);
-        //   WM_SetCallback(hItem, _cb);
+//        p[2] = rt_malloc(38399);
+//        fd = open("/SD/DESKTOP.JPG",O_RDONLY , 0);
+//        read(fd, p[2], 38399);
+//        hItem = WM_GetDialogItem(pMsg->hWin, ID_IMAGE_0);
+//        WM_EnableMemdev(hItem);
+//          IMAGE_SetJPEG(hItem, p[2], 38399);
 
-		for (i = 0; i<4; i++)
+
+		for (i=0;i<5;i++)
 		{
 			hItem = WM_GetDialogItem(pMsg->hWin, ID_BUTTON_0 + i);
-            WM_MOTION_SetMoveable(hItem, WM_CF_MOTION_X, 1);
-            WM_MOTION_SetMoveable(hItem, WM_CF_MOTION_Y, 1);
             WM_EnableMemdev(hItem);
-			WM_MoveTo(hItem, 0 + i * 55, 0);
-            p[i] = rt_malloc(9*1024);
-            fd = open(AppImageTable[i],O_RDONLY , 0);
-            read(fd, p[i], 8192);
-            BUTTON_SetBMPEx(hItem, BUTTON_BI_UNPRESSED, p[i] ,0, 0);
-            close(fd);
-            //rt_free(p[i]);
+			WM_MoveTo(hItem, 10+(i%4)*60, (i/4)*70 + 10);
+            
+            if(AppImageTable[i])
+            {
+                p[i] = rt_malloc(9*1024);
+                fd = open(AppImageTable[i],O_RDONLY , 0);
+                read(fd, p[i], 8192);
+                BUTTON_SetBMPEx(hItem, BUTTON_BI_UNPRESSED, p[i] ,0, 0);
+                close(fd);
+            }
 		}
         hItem = pMsg->hWin;
-	//	WM_MoveTo(hItem, -60, 0);
-
         hItem = WM_GetDialogItem(pMsg->hWin, ID_BUTTON_0);
         
         break;
-        case WM_TOUCH:
-            pState = (const GUI_PID_STATE *)pMsg->Data.p;
-            if (pState) {
-			if (pState->Pressed) {
-			//	WM_SetCaptureMove(pMsg->hWin, pState, 0, 0);
-            }
-            }
-            break;
     case WM_NOTIFY_PARENT:
         Id = WM_GetId(pMsg->hWinSrc);
         NCode = pMsg->Data.v;
@@ -165,7 +134,7 @@ static void _cbDialog(WM_MESSAGE * pMsg) {
         }
         if(msg.exec != RT_NULL)
         {
-     //       rt_mq_send(guimq, &msg, sizeof(msg));
+            rt_mq_send(guimq, &msg, sizeof(msg));
         }
     }
     break;
@@ -184,7 +153,7 @@ WM_HWIN GUI_CreateDesktopDialog(void)
   // // h = GUI_MEMDEV_Create(0, 0, 50, 50);
   //  GUI_MEMDEV_Select(h);
  //   
-    WM_SetCallback(WM_HBKWIN, _cb);
+    //WM_SetCallback(WM_HBKWIN, _cb);
     LCD_SetSizeEx(0, 240, 320);
 	LCD_SetVSizeEx(0, 400, 320);
     WM_MOTION_Enable(1);

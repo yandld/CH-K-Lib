726093a576384cdb5d262b370018958857090123
YangXi <B51439@freescale.com>
2015-01-26 10:49:42 +0800

    Add KL templete

---
+++ Libraries/drivers/K/inc/cmp.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    cmp.h
+  * @author  YANDLD
+  * @version V2.5
+  * @date    2015.1.26
+  * @brief   www.beyondcore.net   http://upcmcu.taobao.com 
+  ******************************************************************************
+  */
+  
+#ifndef __CH_LIB_CMP_H__
+#define __CH_LIB_CMP_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include <stdint.h> 
+
+#endif

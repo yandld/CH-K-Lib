4a6211b7f003926443d282f55bfabe26683446df
YangXi <B51439@freescale.com>
2015-02-04 12:38:59 +0800

    Refine

--- Project/K60核心板-智能车专用/0-工程模板/src/main.c
+++ Project/K60核心板-智能车专用/0-工程模板/src/main.c
@@ -27,22 +27,21 @@
  UART5_RX_PD08_TX_PD09   
 */
  
+ 
 int main(void)
 {
     DelayInit();
     /* 初始化一个引脚 设置为推挽输出 */
-    GPIO_QuickInit(HW_GPIOD, 10, kGPIO_Mode_OPP);
-    GPIO_QuickInit(HW_GPIOC,  3, kGPIO_Mode_OPP);
-
+    GPIO_QuickInit(HW_GPIOE, 6, kGPIO_Mode_OPP);
+    /* 初始化一个串口 使用UART0端口的PTD6引脚和PTD7引脚作为接收和发送，默认设置 baud 115200 */
     UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
     
     printf("HelloWorld!\r\n");
-    
     while(1)
     {
+        
         /* 闪烁小灯 */
-        GPIO_ToggleBit(HW_GPIOD, 10);
-        GPIO_ToggleBit(HW_GPIOC,  3);
+        GPIO_ToggleBit(HW_GPIOE, 6);
         DelayMs(500);
     }
 }

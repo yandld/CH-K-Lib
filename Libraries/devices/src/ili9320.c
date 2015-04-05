65723608af2272638a31f357b96ca1e354636952
YangXi <B51439@freescale.com>
2015-04-05 09:17:33 +0800

    Refine
    Signed-off-by: YangXi <B51439@freescale.com>

--- Libraries/devices/src/ili9320.c
+++ Libraries/devices/src/ili9320.c
@@ -3,7 +3,7 @@
 #include "flexbus.h"
 
 
-#define ILI9320_DEBUG		0
+#define ILI9320_DEBUG		1
 #if ( ILI9320_DEBUG == 1 )
 #define ILI9320_TRACE	printf
 #else
@@ -117,6 +117,7 @@ void ili9320_vline(int ys, int ye, int x, int c)
 
 void ili9320_init(void)
 {
+    uint32_t lcd_id;
     uint32_t gpio_instance;
     /* 减低flexbus总线速度 总线速度太高 不能正确执行读点操作 */
     /* Flexbus Init */
@@ -193,79 +194,129 @@ void ili9320_init(void)
     /* reset */
     gpio_instance = GPIO_QuickInit(HW_GPIOC, 19, kGPIO_Mode_OPP);
     GPIO_WriteBit(gpio_instance, 19, 0); 
-    DelayMs(5);
+    DelayMs(1);
     GPIO_WriteBit(gpio_instance, 19, 1);
-    DelayMs(5); 
+    DelayMs(1);
     
-    //LCD_WR_REG(0xe5,0x8000);  // Set the internal vcore voltage    
-    write_reg(0x00,0x0001);  // start OSC    
-    write_reg(0x2b,0x0010);  //Set the frame rate as 80 when the internal resistor is used for oscillator circuit    
-    write_reg(0x01,0x0100);  //s720  to  s1 ; G1 to G320    
-    write_reg(0x02,0x0700);  //set the line inversion    
-    //LCD_WR_REG(0x03,0x1018);  //65536 colors     
-    write_reg(0x03,0x1030);   
-    //横屏
-    #ifdef LCD_USE_HORIZONTAL
-    write_reg(0x03,(0<<5)|(0<<4)|(1<<3)|(1<<12));
-    #else
-    write_reg(0x03,(1<<5)|(1<<4)|(0<<3)|(1<<12));
-    #endif
+    lcd_id = ili9320_get_id();
+    
+    switch(lcd_id)
+    {
+        case 0x9320:
+            write_reg(0xe5,0x8000);  // Set the internal vcore voltage    
+            write_reg(0x00,0x0001);  // start OSC    
+            write_reg(0x2b,0x0010);  //Set the frame rate as 80 when the internal resistor is used for oscillator circuit    
+            write_reg(0x01,0x0100);  //s720  to  s1 ; G1 to G320    
+            write_reg(0x02,0x0700);  //set the line inversion    
+            //LCD_WR_REG(0x03,0x1018);  //65536 colors     
+            write_reg(0x03,0x1030);   
+            //横屏
+            #ifdef LCD_USE_HORIZONTAL
+            write_reg(0x03,(0<<5)|(0<<4)|(1<<3)|(1<<12));
+            #else
+            write_reg(0x03,(1<<5)|(1<<4)|(0<<3)|(1<<12));
+            #endif
 
-    write_reg(0x04,0x0000);   
-    write_reg(0x08,0x0202);  //specify the line number of front and back porch periods respectively    
-    write_reg(0x09,0x0000);   
-    write_reg(0x0a,0x0000);   
-    write_reg(0x0c,0x0000);  //select  internal system clock    
-    write_reg(0x0d,0x0000);   
-    write_reg(0x0f,0x0000);    
-    write_reg(0x50,0x0000);  //0x50 -->0x53 set windows adress    
-    write_reg(0x51,0x00ef);   
-    write_reg(0x52,0x0000);   
-    write_reg(0x53,0x013f);   
-    write_reg(0x60,0x2700);   
-    write_reg(0x61,0x0001);   
-    write_reg(0x6a,0x0000);   
-    write_reg(0x80,0x0000);   
-    write_reg(0x81,0x0000);   
-    write_reg(0x82,0x0000);   
-    write_reg(0x83,0x0000);   
-    write_reg(0x84,0x0000);   
-    write_reg(0x85,0x0000);   
-    write_reg(0x90,0x0010);   
-    write_reg(0x92,0x0000);   
-    write_reg(0x93,0x0003);   
-    write_reg(0x95,0x0110);   
-    write_reg(0x97,0x0000);   
-    write_reg(0x98,0x0000);    
- 
-    //power setting function    
-    write_reg(0x10,0x0000);   
-    write_reg(0x11,0x0000);   
-    write_reg(0x12,0x0000);   
-    write_reg(0x13,0x0000);   
-    DelayMs(20);   
-    write_reg(0x10,0x17b0);   
-    write_reg(0x11,0x0004);   
-    DelayMs(5);   
-    write_reg(0x12,0x013e);   
-    DelayMs(5);   
-    write_reg(0x13,0x1f00);   
-    write_reg(0x29,0x000f);   
-    DelayMs(5);   
-    write_reg(0x20,0x0000);   
-    write_reg(0x21,0x0000);   
- 
-    //initializing function 2    
-    write_reg(0x30,0x0204);   
-    write_reg(0x31,0x0001);   
-    write_reg(0x32,0x0000);   
-    write_reg(0x35,0x0206);   
-    write_reg(0x36,0x0600);   
-    write_reg(0x37,0x0500);   
-    write_reg(0x38,0x0505);   
-    write_reg(0x39,0x0407);   
-    write_reg(0x3c,0x0500);   
-    write_reg(0x3d,0x0503);   
+            write_reg(0x04,0x0000);   
+            write_reg(0x08,0x0202);  
+            write_reg(0x09,0x0000);   
+            write_reg(0x0a,0x0000);   
+            write_reg(0x0c,0x0000);
+            write_reg(0x0d,0x0000);   
+            write_reg(0x0f,0x0000);    
+            write_reg(0x50,0x0000);
+            write_reg(0x51,0x00ef);   
+            write_reg(0x52,0x0000);   
+            write_reg(0x53,0x013f);   
+            write_reg(0x60,0x2700);   
+            write_reg(0x61,0x0001);   
+            write_reg(0x6a,0x0000);   
+            write_reg(0x80,0x0000);   
+            write_reg(0x81,0x0000);   
+            write_reg(0x82,0x0000);   
+            write_reg(0x83,0x0000);   
+            write_reg(0x84,0x0000);   
+            write_reg(0x85,0x0000);   
+            write_reg(0x90,0x0010);   
+            write_reg(0x92,0x0000);   
+            write_reg(0x93,0x0003);   
+            write_reg(0x95,0x0110);   
+            write_reg(0x97,0x0000);   
+            write_reg(0x98,0x0000);    
+         
+            write_reg(0x10,0x0000);   
+            write_reg(0x11,0x0000);   
+            write_reg(0x12,0x0000);   
+            write_reg(0x13,0x0000);   
+            DelayMs(20);   
+            write_reg(0x10,0x17b0);   
+            write_reg(0x11,0x0004);   
+            DelayMs(1);   
+            write_reg(0x12,0x013e);   
+            DelayMs(1);   
+            write_reg(0x13,0x1f00);   
+            write_reg(0x29,0x000f);   
+            DelayMs(1);   
+            write_reg(0x20,0x0000);   
+            write_reg(0x21,0x0000);   
+         
+            write_reg(0x30,0x0204);   
+            write_reg(0x31,0x0001);   
+            write_reg(0x32,0x0000);   
+            write_reg(0x35,0x0206);   
+            write_reg(0x36,0x0600);   
+            write_reg(0x37,0x0500);   
+            write_reg(0x38,0x0505);   
+            write_reg(0x39,0x0407);   
+            write_reg(0x3c,0x0500);   
+            write_reg(0x3d,0x0503);   
+            break;
+        case 0x8989:
+            write_reg(0x0000,0x0001);DelayMs(1);
+            write_reg(0x0003,0xA8A4);DelayMs(1);
+            write_reg(0x000C,0x0000);DelayMs(1);    
+            write_reg(0x000D,0x080C);DelayMs(1);    
+            write_reg(0x000E,0x2B00);DelayMs(1);    
+            write_reg(0x001E,0x00B0);DelayMs(1);    
+            write_reg(0x0001,0x2B3F);DelayMs(1);
+            write_reg(0x0002,0x0600);DelayMs(1); 
+            write_reg(0x0010,0x0000);DelayMs(1); 
+            write_reg(0x0011,0x6070);DelayMs(1);
+            write_reg(0x0005,0x0000);DelayMs(1); 
+            write_reg(0x0006,0x0000);DelayMs(1); 
+            write_reg(0x0016,0xEF1C);DelayMs(1); 
+            write_reg(0x0017,0x0003);DelayMs(1); 
+            write_reg(0x0007,0x0233);DelayMs(1);
+            write_reg(0x000B,0x0000);DelayMs(1); 
+            write_reg(0x000F,0x0000);DelayMs(1);
+            write_reg(0x0041,0x0000);DelayMs(1); 
+            write_reg(0x0042,0x0000);DelayMs(1); 
+            write_reg(0x0048,0x0000);DelayMs(1); 
+            write_reg(0x0049,0x013F);DelayMs(1); 
+            write_reg(0x004A,0x0000);DelayMs(1); 
+            write_reg(0x004B,0x0000);DelayMs(1); 
+            write_reg(0x0044,0xEF00);DelayMs(1); 
+            write_reg(0x0045,0x0000);DelayMs(1); 
+            write_reg(0x0046,0x013F);DelayMs(1); 
+            write_reg(0x0030,0x0707);DelayMs(1); 
+            write_reg(0x0031,0x0204);DelayMs(1); 
+            write_reg(0x0032,0x0204);DelayMs(1); 
+            write_reg(0x0033,0x0502);DelayMs(1); 
+            write_reg(0x0034,0x0507);DelayMs(1); 
+            write_reg(0x0035,0x0204);DelayMs(1); 
+            write_reg(0x0036,0x0204);DelayMs(1); 
+            write_reg(0x0037,0x0502);DelayMs(1); 
+            write_reg(0x003A,0x0302);DelayMs(1); 
+            write_reg(0x003B,0x0302);DelayMs(1); 
+            write_reg(0x0023,0x0000);DelayMs(1); 
+            write_reg(0x0024,0x0000);DelayMs(1); 
+            write_reg(0x0025,0x8000);DelayMs(1); 
+            write_reg(0x004f,0);
+            write_reg(0x004e,0);
+            break;
+        default:
+            break;
+    }
     
     //开启显示 
     ILI9320_TRACE("ID:0x%X\r\n", ili9320_get_id());

ec2dc2cf65ee34e7b0da52ceab0d89f3b6bad0bf
YangXi <B51439@freescale.com>
2015-04-29 17:44:21 +0800

    Refine OV7620

--- Project/超核K60-K64开发板-应用例程/02-OV7620摄像头实验/src/main.c
+++ Project/超核K60-K64开发板-应用例程/02-OV7620摄像头实验/src/main.c
@@ -31,39 +31,40 @@ static void UserApp(void);
 /* 摄像头场中断处理函数 */
 void OV7620_ISR(uint32_t pinArray)
 {
+    static OV7620_Status states;
+    static uint32_t href_counter;
+    
     if(pinArray & (1<<BOARD_OV7620_VSYNC_PIN))
     {
-    static uint8_t status = TRANSFER_IN_PROCESS;  
-    switch(status)
-    {
-        case TRANSFER_IN_PROCESS: /* 正在传输 */
-            /* 查看DMA是否完成传送 */
-            if(DMA_IsMajorLoopComplete(HW_DMA_CH2) == 0)
-            {
+        switch(states)
+        {
+            case TRANSFER_IN_PROCESS:
                 GPIO_ITDMAConfig(BOARD_OV7620_VSYNC_PORT, BOARD_OV7620_VSYNC_PIN, kGPIO_IT_RisingEdge, false);
-                /* 开始处理用户 函数 */
+                GPIO_ITDMAConfig(BOARD_OV7620_PCLK_PORT, BOARD_OV7620_PCLK_PIN, kGPIO_DMA_RisingEdge, false);
+                GPIO_ITDMAConfig(BOARD_OV7620_HREF_PORT, BOARD_OV7620_HREF_PIN, kGPIO_IT_RisingEdge, false);
                 UserApp();
-                /* 用户函数处理完毕 */
-                status = NEXT_FRAME;
-                /* 处理完用户函数后 继续开启场中断 准备下一场数据到达 */
-                GPIO_ITDMAConfig(BOARD_OV7620_VSYNC_PORT, BOARD_OV7620_VSYNC_PIN, kGPIO_IT_RisingEdge, true);  
-            }
-            else
-            {
-                status = TRANSFER_IN_PROCESS;
-            }
-            break;
-        case NEXT_FRAME:
-            /* 传输完成 复位buffer地址 开始下一场传输 */
-            DMA_SetDestAddress(HW_DMA_CH2, (uint32_t)CCDBuffer[0]);
-            DMA_EnableRequest(HW_DMA_CH2);
-            status =  TRANSFER_IN_PROCESS;
-            break;
-        default:
-            break;
-    }
-    }
+                GPIO_ITDMAConfig(BOARD_OV7620_VSYNC_PORT, BOARD_OV7620_VSYNC_PIN, kGPIO_IT_RisingEdge, true);                 //DMA_EnableRequest(HW_DMA_CH2);
+                states = NEXT_FRAME;
+                //href_counter = 0;
+                break;
+            case NEXT_FRAME:
+                    GPIO_ITDMAConfig(BOARD_OV7620_PCLK_PORT, BOARD_OV7620_PCLK_PIN, kGPIO_DMA_RisingEdge, true); 
+                    GPIO_ITDMAConfig(BOARD_OV7620_HREF_PORT, BOARD_OV7620_HREF_PIN, kGPIO_IT_RisingEdge, true);  
+                    DMA_SetDestAddress(HW_DMA_CH2, (uint32_t)CCDBuffer[0]);
+                    states = TRANSFER_IN_PROCESS;
+                    href_counter = 0;
+                    
+                break;
+        }
 
+    }
+    
+    if(pinArray & (1<<BOARD_OV7620_HREF_PIN))
+    {
+        DMA_SetDestAddress(HW_DMA_CH2, (uint32_t)CCDBuffer[href_counter++]);
+        DMA_SetMajorLoopCounter(HW_DMA_CH2, OV7620_W);
+        DMA_EnableRequest(HW_DMA_CH2);        
+    }
 }
 
 static uint16_t RGB2COLOR(uint8_t RR,uint8_t GG,uint8_t BB)
@@ -91,12 +92,12 @@ static void OV7620_Init(void)
 {
     DMA_InitTypeDef DMA_InitStruct1 = {0};
     uint32_t i;
-    //把开辟的内存池付给指针
+
     for(i=0;i< OV7620_H;i++)
     {
         CCDBuffer[i] = (uint8_t *) &CCDBufferPool[i*OV7620_W];
     }
-    /* 场中断  行中断 像素中断 */
+
     GPIO_QuickInit(BOARD_OV7620_PCLK_PORT, BOARD_OV7620_PCLK_PIN, kGPIO_Mode_IPD);
     GPIO_QuickInit(BOARD_OV7620_VSYNC_PORT, BOARD_OV7620_VSYNC_PIN, kGPIO_Mode_IPD);
     GPIO_QuickInit(BOARD_OV7620_HREF_PORT, BOARD_OV7620_HREF_PIN, kGPIO_Mode_IPD);
@@ -106,14 +107,14 @@ static void OV7620_Init(void)
         GPIO_QuickInit(HW_GPIOA, BOARD_OV7620_DATA_OFFSET+i, kGPIO_Mode_IFT);
     }
     GPIO_CallbackInstall(BOARD_OV7620_VSYNC_PORT, OV7620_ISR);
-    GPIO_ITDMAConfig(BOARD_OV7620_HREF_PORT, BOARD_OV7620_HREF_PIN, kGPIO_DMA_RisingEdge, true);
+    GPIO_ITDMAConfig(BOARD_OV7620_HREF_PORT, BOARD_OV7620_HREF_PIN, kGPIO_IT_RisingEdge, true);
     GPIO_ITDMAConfig(BOARD_OV7620_VSYNC_PORT, BOARD_OV7620_VSYNC_PIN, kGPIO_IT_RisingEdge, true);
-  //  GPIO_ITDMAConfig(BOARD_OV7620_PCLK_PORT, BOARD_OV7620_PCLK_PIN, kGPIO_DMA_RisingEdge); //实际并没有用到
+    GPIO_ITDMAConfig(BOARD_OV7620_PCLK_PORT, BOARD_OV7620_PCLK_PIN, kGPIO_DMA_RisingEdge, true); //实际并没有用到
     DMA_InitStruct1.chl = HW_DMA_CH2;
     DMA_InitStruct1.chlTriggerSource = PORTA_DMAREQ;
     DMA_InitStruct1.triggerSourceMode = kDMA_TriggerSource_Normal;
-    DMA_InitStruct1.minorLoopByteCnt = OV7620_W;
-    DMA_InitStruct1.majorLoopCnt = OV7620_H;
+    DMA_InitStruct1.minorLoopByteCnt = 1;
+    DMA_InitStruct1.majorLoopCnt = OV7620_W;
     
     DMA_InitStruct1.sAddr = (uint32_t)&PTA->PDIR + BOARD_OV7620_DATA_OFFSET/8;
     DMA_InitStruct1.sLastAddrAdj = 0;
@@ -126,21 +127,20 @@ static void OV7620_Init(void)
     DMA_InitStruct1.dDataWidth = kDMA_DataWidthBit_8;
 
     DMA_Init(&DMA_InitStruct1);
-    /* 开始传输 */
-    DMA_EnableRequest(HW_DMA_CH2); 
 }
 
-void OV7620_Reset(void)
-{
-    SCCB_WriteSingleRegister(HW_I2C0, OV7620_I2C_CHIP_ADDR, 0x12, 0x80);
-}
+
 
 void SCCB_Init(void)
 {
     I2C_QuickInit(I2C0_SCL_PB00_SDA_PB01, 100*1000);
-    /* 只是复位 全部做默认设置 */
-    /* Yandld: 如果是默认出厂配置, 则无需配置SCCB */
-    OV7620_Reset();
+
+    /* reset */
+    SCCB_WriteSingleRegister(HW_I2C0, OV7620_I2C_CHIP_ADDR, 0x12, 0x80);
+    
+    /* PCLK gated by HREF */
+    SCCB_WriteSingleRegister(HW_I2C0, OV7620_I2C_CHIP_ADDR, 0x71, 0x40);
+
     DelayMs(10);
     /* 默认配置如下 */
     /* 0x12,0x64
@@ -156,11 +156,14 @@ int main(void)
     
     printf("OV7620 test\r\n");
     
-    ili9320_init();
     SRAM_Init();
+    ili9320_init();
+    
+    
     /* 摄像头速度非常快 把FLexbus 总线速度调到最高 */
     SIM->CLKDIV1 &= ~SIM_CLKDIV1_OUTDIV3_MASK;
-    SIM->CLKDIV1 |= SIM_CLKDIV1_OUTDIV3(0);
+    SIM->CLKDIV1 |= SIM_CLKDIV1_OUTDIV3(1);
+    
     OV7620_Init();
     SCCB_Init();
     while(1)

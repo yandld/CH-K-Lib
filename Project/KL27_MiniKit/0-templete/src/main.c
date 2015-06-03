07a1b82d13a92509e8681847dd5f329c30294c0e
YangXi <B51439@freescale.com>
2015-06-03 13:44:52 +0800

    Refine MiniKit

--- Project/KL27_MiniKit/0-templete/src/main.c
+++ Project/KL27_MiniKit/0-templete/src/main.c
@@ -4,12 +4,24 @@
 #include "common.h"
 #include "lpuart.h"
 
-
+#define LED0_PORT   HW_GPIOC
+#define LED0_PIN    3
+#define LED1_PORT   HW_GPIOA
+#define LED1_PIN    1
+#define LED2_PORT   HW_GPIOE
+#define LED2_PIN    0
+#define LED3_PORT   HW_GPIOA
+#define LED3_PIN    2
+    
 int main(void)
 {
     DelayInit();
     
-    GPIO_QuickInit(HW_GPIOC, 3, kGPIO_Mode_OPP);
+    GPIO_QuickInit(LED0_PORT, LED0_PIN, kGPIO_Mode_OPP);
+    GPIO_QuickInit(LED1_PORT, LED1_PIN, kGPIO_Mode_OPP);
+    GPIO_QuickInit(LED2_PORT, LED2_PIN, kGPIO_Mode_OPP);
+    GPIO_QuickInit(LED3_PORT, LED3_PIN, kGPIO_Mode_OPP);
+    
     LPUART_QuickInit(LPUART0_RX_D06_TX_D07, 115200);
 
     printf("HelloWorld!\r\n");
@@ -18,7 +30,11 @@ int main(void)
 
     while(1)
     {
-        putchar(getchar());
+        GPIO_ToggleBit(LED0_PORT, LED0_PIN);
+        GPIO_ToggleBit(LED1_PORT, LED1_PIN);
+        GPIO_ToggleBit(LED2_PORT, LED2_PIN);
+        GPIO_ToggleBit(LED3_PORT, LED3_PIN);
+        DelayMs(200);
     }
 }
 

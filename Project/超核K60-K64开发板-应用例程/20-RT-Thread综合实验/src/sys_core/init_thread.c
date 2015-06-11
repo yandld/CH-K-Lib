4ce5b99021aeb38f6910478655ec7bec1097cf5b
yandld <yandld@126.com>
2015-06-11 21:22:52 +0800

    Refine

--- Project/超核K60-K64开发板-应用例程/20-RT-Thread综合实验/src/sys_core/init_thread.c
+++ Project/超核K60-K64开发板-应用例程/20-RT-Thread综合实验/src/sys_core/init_thread.c
@@ -40,7 +40,7 @@ void init_thread_entry(void* parameter)
     rt_hw_spi_init();
     rt_hw_rtc_init();
     rt_hw_dflash_init("dflash0");
-    
+    rt_hw_sd_init();
     /* start usbd core thread */
     tid = rt_thread_create("usbd", usb_thread_entry, RT_NULL, (1024*1), 0x15, 20);                                                      
     rt_thread_startup(tid);	

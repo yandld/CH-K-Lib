5dc12f3947e6bb80e4225d0711dbc31556f0c1ca
YangXi <B51439@freescale.com>
2015-06-26 11:29:32 +0800

    Refine

---
+++ Project/超核K60-K64开发板-应用例程/24-Keil绝对地址定位/src/main.c
@@ -0,0 +1,30 @@
+#include "gpio.h"
+#include "common.h"
+#include "uart.h"
+#include "dma.h"
+#include "sram.h"
+#include <absacc.h> 
+
+int test(void) __attribute__((section(".ARM.__at_0x8000")));
+int test(void)
+{
+    
+}
+
+const char var __at (0x8100);
+
+int main(void)
+{
+
+    UART_QuickInit(UART0_RX_PD06_TX_PD07, 115200);
+    printf("DMA UART transmit test\r\n");
+    printf("fun addr:0x%08X\r\n", (uint32_t)test);
+    printf("var addr:0x%08X\r\n", (uint32_t)&var);
+    while(1)
+    {
+        GPIO_ToggleBit(HW_GPIOE, 6);
+        DelayMs(200);
+    }
+}
+
+

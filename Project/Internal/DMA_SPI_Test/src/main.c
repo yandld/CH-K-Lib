6b6cf204a77471a573342f53cba0774f3ca93497
YangXi <B51439@freescale.com>
2015-07-31 14:30:59 +0800

    Refine

--- Project/Internal/DMA_SPI_Test/src/main.c
+++ Project/Internal/DMA_SPI_Test/src/main.c
@@ -6,8 +6,8 @@
 #include "w25qxx.h"
 
 
-#define DMA_SPI_TX_CH   0
-#define DMA_SPI_RX_CH   1
+#define DMA_SPI_TX_CH   (0)
+#define DMA_SPI_RX_CH   (1)
 
 static uint32_t gProbleCmd[] = 
 {
@@ -21,8 +21,7 @@ static uint32_t gProbleCmd[] =
 
 
 
-
-void SPI_xfer(uint32_t instance, uint32_t *buf_in, uint32_t *buf_out, uint32_t size)
+void SPI_xfer(uint32_t *buf_in, uint32_t *buf_out, uint32_t size)
 {
     int i;
     
@@ -33,100 +32,61 @@ void SPI_xfer(uint32_t instance, uint32_t *buf_in, uint32_t *buf_out, uint32_t s
     }
     buf_in[size-1] &= ~SPI_PUSHR_CONT_MASK;
     
-    DMA_SetSourceAddress(DMA_SPI_TX_CH, (uint32_t)buf_in);
-    DMA_SetDestAddress(DMA_SPI_RX_CH, (uint32_t)buf_out);
-    DMA_SetMajorLoopCounter(DMA_SPI_TX_CH, size);
-    DMA_SetMajorLoopCounter(DMA_SPI_RX_CH, size);
+    /* set SPI Tx DMA ch's source addr */
+    DMA0->TCD[DMA_SPI_TX_CH].SADDR = (uint32_t)buf_in;
+    /* set SPI Rx DMA ch's dest addr */
+    DMA0->TCD[DMA_SPI_RX_CH].DADDR = (uint32_t)buf_out;
+
+    /* set major loop */
+    DMA0->TCD[DMA_SPI_TX_CH].BITER_ELINKNO = DMA_BITER_ELINKNO_BITER(size);
+    DMA0->TCD[DMA_SPI_RX_CH].BITER_ELINKNO = DMA_BITER_ELINKNO_BITER(size);
     
-    DMA_EnableRequest(1);
-    DMA_EnableRequest(0);
+    /* enable request */
+    DMA0->ERQ = (1 << DMA_SPI_TX_CH) | (1 << DMA_SPI_RX_CH);
 }
 
-static void UART_SPI_Config(uint32_t instance)
+static void UART_SPI_Config(void)
 {
-    /* MOSI */
-    DMA_InitTypeDef DMA_InitStruct1 = {0};
-    DMA_InitStruct1.chl = DMA_SPI_TX_CH;
-    DMA_InitStruct1.chlTriggerSource = SPI2_TRAN_DMAREQ;
-    DMA_InitStruct1.triggerSourceMode = kDMA_TriggerSource_Normal;
-    DMA_InitStruct1.minorLoopByteCnt = 4;
-    DMA_InitStruct1.majorLoopCnt = 0;
+    /* enable DMA and DMAMUX clock */
+    SIM->SCGC6 |= SIM_SCGC6_DMAMUX_MASK;
+    SIM->SCGC7 |= SIM_SCGC7_DMA_MASK;
+
+    /* configure DMAMUX */
+    DMAMUX->CHCFG[DMA_SPI_TX_CH]= DMAMUX_CHCFG_ENBL_MASK | DMAMUX_CHCFG_SOURCE(SPI2_TRAN_DMAREQ);
+    DMAMUX->CHCFG[DMA_SPI_RX_CH]= DMAMUX_CHCFG_ENBL_MASK | DMAMUX_CHCFG_SOURCE(SPI2_REV_DMAREQ);
     
-    DMA_InitStruct1.sAddr = NULL;
-    DMA_InitStruct1.sLastAddrAdj = 0; 
-    DMA_InitStruct1.sAddrOffset = 4;
-    DMA_InitStruct1.sDataWidth = kDMA_DataWidthBit_32;
-    DMA_InitStruct1.sMod = kDMA_ModuloDisable;
     
-    DMA_InitStruct1.dAddr = (uint32_t)&SPI2->PUSHR; 
-    DMA_InitStruct1.dLastAddrAdj = 0;
-    DMA_InitStruct1.dAddrOffset = 0;
-    DMA_InitStruct1.dDataWidth = kDMA_DataWidthBit_32;
-    DMA_InitStruct1.dMod = kDMA_ModuloDisable;
-    DMA_Init(&DMA_InitStruct1);
+    /* source configuration MOSI */
+    DMA0->TCD[DMA_SPI_TX_CH].SADDR = NULL;
+    DMA0->TCD[DMA_SPI_TX_CH].ATTR |= DMA_ATTR_SSIZE(2);
+    DMA0->TCD[DMA_SPI_TX_CH].SOFF= 4;
+    DMA0->TCD[DMA_SPI_TX_CH].SLAST = 0;
     
-    /* SPI MISO */
-    DMA_InitStruct1.chl = DMA_SPI_RX_CH;
-    DMA_InitStruct1.chlTriggerSource = SPI2_REV_DMAREQ;
-    DMA_InitStruct1.triggerSourceMode = kDMA_TriggerSource_Normal;
-    DMA_InitStruct1.minorLoopByteCnt = 4;
-    DMA_InitStruct1.majorLoopCnt = 0;
+    /* destination configuration MOSI */
+    DMA0->TCD[DMA_SPI_TX_CH].DADDR = (uint32_t)&SPI2->PUSHR;
+    DMA0->TCD[DMA_SPI_TX_CH].ATTR |= DMA_ATTR_DSIZE(2);
+    DMA0->TCD[DMA_SPI_TX_CH].DOFF= 0;
+    DMA0->TCD[DMA_SPI_TX_CH].DLAST_SGA= 0;
     
-    DMA_InitStruct1.sAddr = (uint32_t)&SPI2->POPR;
-    DMA_InitStruct1.sLastAddrAdj = 0; 
-    DMA_InitStruct1.sAddrOffset = 0;
-    DMA_InitStruct1.sDataWidth = kDMA_DataWidthBit_32;
-    DMA_InitStruct1.sMod = kDMA_ModuloDisable;
+    /* set CITER and BITER to maximum value MOSI */
+    DMA0->TCD[DMA_SPI_TX_CH].NBYTES_MLNO = 4;
     
-    DMA_InitStruct1.dAddr = NULL; 
-    DMA_InitStruct1.dLastAddrAdj = 0;
-    DMA_InitStruct1.dAddrOffset = 4;
-    DMA_InitStruct1.dDataWidth = kDMA_DataWidthBit_32;
-    DMA_InitStruct1.dMod = kDMA_ModuloDisable;
-    DMA_Init(&DMA_InitStruct1);
+    /* source configuration MISO */
+    DMA0->TCD[DMA_SPI_RX_CH].SADDR = (uint32_t)&SPI2->POPR;
+    DMA0->TCD[DMA_SPI_RX_CH].ATTR |= DMA_ATTR_SSIZE(2);
+    DMA0->TCD[DMA_SPI_RX_CH].SOFF= 0;
+    DMA0->TCD[DMA_SPI_RX_CH].SLAST = 0;
     
+    /* destination configuration MISO */
+    DMA0->TCD[DMA_SPI_RX_CH].DADDR = NULL;
+    DMA0->TCD[DMA_SPI_RX_CH].ATTR |= DMA_ATTR_DSIZE(2);
+    DMA0->TCD[DMA_SPI_RX_CH].DOFF= 4;
+    DMA0->TCD[DMA_SPI_RX_CH].DLAST_SGA= 0;
+    
+    /* set CITER and BITER to maximum value MISO */
+    DMA0->TCD[DMA_SPI_RX_CH].NBYTES_MLNO = 4;
     
 }
-
-
-static int w25qxx_test(void)
-{
-    uint32_t i, block, buf_size,j;
-    static uint8_t buf[4*1024];
-    
-    printf("spi flash id:0x%X\r\n", w25qxx_get_id());
-    buf_size = sizeof(buf);
-    
-    block = 512;
-    
-    /* erase chip */
-    printf("erase all chips...\r\n");
-    w25qxx_erase_chip();
-    printf("erase complete\r\n");
-    
-    for(i=0; i<block; i++)
-    {
-        printf("verify addr:0x%X(%d)...\r\n", i*buf_size, i);
-        for(j=0;j<sizeof(buf);j++)
-        {
-            buf[j] = j % 0xFF;
-        }
-        w25qxx_write(i*block, buf, buf_size);
-        memset(buf, buf_size, 0);
-        w25qxx_read(i*block, buf, buf_size);
-        
-        /* varify */
-        for(j=0;j<sizeof(buf);j++)
-        {
-            if(buf[j] != (j%0xFF))
-            {
-                printf("%d error\r\n", j);
-            }
-        }
-    }
-    return 0;
-}
-
 
 
 int main(void)
@@ -143,47 +103,21 @@ int main(void)
     SPI_EnableTxFIFO(HW_SPI2, true);
     SPI_EnableRxFIFO(HW_SPI2, true);
     
-    UART_SPI_Config(2);
+    UART_SPI_Config();
     SPI_ITDMAConfig(HW_SPI2, kSPI_DMA_TFFF, true);
     SPI_ITDMAConfig(HW_SPI2, kSPI_DMA_RFDF, true);
 
-    SPI_xfer(HW_SPI2, gProbleCmd, rev_buf, 6);
-    
-    DelayMs(100);
-    i = DMA_GetMajorLoopCount(DMA_SPI_TX_CH);
-    printf("cnt:%d\r\n", i);
-    i = DMA_GetMajorLoopCount(DMA_SPI_RX_CH);
-    printf("cnt:%d\r\n", i);
-    for(i=0; i<ARRAY_SIZE(rev_buf); i++)
-    {
-        printf("0x%X  ", rev_buf[i]);
-    }
-    while(1);
-    
-    struct w25qxx_init_t init;
-    
-    
-    if(w25qxx_init(&init))
-    {
-        printf("w25qxx device no found!\r\n");
-    }
-    else
-    {
-        printf("spi_flash found id:0x%X\r\n", w25qxx_get_id());
-    }
+    SPI_xfer(gProbleCmd, rev_buf, 6);
 
-    w25qxx_test();
+    DelayMs(100);
     
-    printf("w252qxx test complete\r\n");
+    printf("spi flash id:0x%X\r\n", (rev_buf[5]<<8) + rev_buf[4]);
+    
     while(1)
     {
-        /* 通过串口使用dma功能实现数据发送 */
-    //   UART_SendWithDMA(HW_DMA_CH2, (const uint8_t*)String1, sizeof(String1));
-        /* 等待DMA传输结束 */
-     //   while(DMA_IsMajorLoopComplete(HW_DMA_CH2));
-        GPIO_ToggleBit(HW_GPIOE, 6);
-        DelayMs(200);
+        
     }
+
 }
 
 

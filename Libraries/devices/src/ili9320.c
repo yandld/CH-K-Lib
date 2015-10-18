98abbb09b66eee457eab4ac6dd1cc21ccc098f77
yandld <yandld@126.com>
2015-10-18 22:56:47 +0800

    Refine

--- Libraries/devices/src/ili9320.c
+++ Libraries/devices/src/ili9320.c
@@ -5,12 +5,106 @@
 #include "flexbus.h"
 #include "common.h"
 
-uint32_t lcd_id;
+
+typedef struct 
+{
+    uint32_t id;
+    uint32_t cmd_gram;
+    uint32_t cmd_setx;
+    uint32_t cmd_sety;
+}lcd_t;
+
+static lcd_t lcd_dev;
+
 
 //FlexBus总线定义
 #define ILI9320_BASE                        (0x70000000)
 #define ILI9320_CMD_BASE                    (0x70000000)
 #define ILI9320_DATA_BASE                   (0x78000000)
+
+/* ILI9341 registers */
+#define SWRESET 	0x01 //Software Reset
+#define RDDIDIF 	0x04 //Read Display Identification Information
+#define RDDST   	0x09 //Read Display Status
+#define RDDPM   	0x0A //Read Display Power Mode
+#define RDDMADCTL	0x0B //Read Display MADCTL
+#define RDDCOLMOD	0x0C //Read Display Pixel Format
+#define RDDIM		0x0D //Read Display Image Mode
+#define RDDSM		0x0E //Read Display Signal Mode
+#define RDDSDR		0x0F //Read Display Self-Diagnostic Result
+#define SPLIN		0x10 //Enter Sleep Mode
+#define SLPOUT		0x11 //Sleep Out
+#define PTLON		0x12 //Partial Mode On
+#define NORON		0x13 //Normal Display Mode On
+#define DINVOFF		0x20 //Display Inversion OFF
+#define DINVON		0x21 //Display Inversion ON
+#define GAMSET      0x26 //Gamma Set
+#define DISPOFF		0x28 //Display OFF
+#define DISPON		0x29 //Display ON
+#define CASET		0x2A //Column Address Set
+#define PASET		0x2B //Page Address Set
+#define RAMWR		0x2C //Memory Write
+#define RGBSET		0x2D //Color Set
+#define RAMRD       0x2E //Memory Read
+#define PLTAR		0x30 //Partial Area
+#define VSCRDEF		0x33 //Vertical Scrolling Definition
+#define TEOFF		0x34 //Tearing Effect Line OFF
+#define TEON		0x35 //Tearing Effect Line ON
+#define MADCTL		0x36 //Memory Access Control
+#define VSCRSADD	0x37 //Vertical Scrolling Start Address
+#define IDMOFF		0x38 //Idle Mode OFF
+#define IDMON		0x39 //Idle Mode ON
+#define PIXSET		0x3A //Pixel Format Set
+#define RAMWRC      0x3C //Write Memory Continue
+#define RAMRDC		0x3E //Read  Memory Continue
+#define STTS		0x44 //Set Tear Scanline 
+#define GTSL		0x45 //Get Scan line
+#define WRDISBV		0x51 //Write Display Brightness
+#define RDDISBV		0x52 //Read Display Brightness Value
+#define WRCTRLD		0x53 //Write Control Display
+#define RDCTRLD		0x54 //Read Control Display
+#define WRCABC		0x55 //Write Content Adaptive Brightness Control
+#define RDCABC		0x56 //Read Content Adaptive Brightness Control
+#define WRCABCMIN	0x5E //Write CABC Minimum Brightness
+#define RDCABCMIN	0x5F //Read CABC Minimum Brightnes
+#define RDID1		0xDA //Read ID1
+#define RDID2		0xDB //Read ID2
+#define RDID3		0xDC //Read ID3
+#define IFMODE		0xB0 //Interface Mode Control
+#define FRMCTR1		0xB1 //Frame Rate Control (In Normal Mode / Full colors
+#define FRMCTR2		0xB2 //Frame Rate Control (In Idle Mode / 8l colors)
+#define FRMCTR3		0xB3 //Frame Rate Control (In Partial Mode / Full colors)
+#define INVTR		0xB4 //Display Inversion Control
+#define PRCTR		0xB5 //Blanking Porch
+#define DISCTRL		0xB6 //Display Function Control
+#define ETMOD		0xB7 //Entry Mode Set
+#define BKCTL1		0xB8 //Backlight Control 1 
+#define BKCTL2		0xB9 //Backlight Control 2 
+#define BKCTL3		0xBA //Backlight Control 3 
+#define BKCTL4		0xBB //Backlight Control 4 
+#define BKCTL5		0xBC //Backlight Control 5
+#define BKCTL7		0xBE //Backlight Control 7
+#define BKCTL8		0xBF //Backlight Control 8
+#define PWCTRL1		0xC0 //Power Control 1
+#define PWCTRL2		0xC1 //Power Control 2
+#define VMCTRL1		0xC5 //VCOM Control 1
+#define VMCTRL2		0xC7 //VCOM Control 2
+#define NVMWR		0xD0 //NV Memory Write
+#define NVMPKEY		0xD1 //NV Memory Protection Key
+#define RDNVM		0xD2 //NV Memory Status Read
+#define RDID4		0xD3 //Read ID4
+#define PGAMCTRL	0xE0 //Positive Gamma Control
+#define NGAMCTRL	0xE1 //Negative Gamma Correction
+#define DGAMCTRL1	0xE2 //Digital Gamma Control 1
+#define DGAMCTRL2	0xE3 //Digital Gamma Control 2
+#define IFCTL		0xF6 //16bits Data Format Selection
+#define PWCTLA		0xCB //Power control A 
+#define PWCTLB		0xCF //Power control B 
+#define DTIMCTLA	0xE8 //Driver timing control A 
+#define DTIMCTLB	0xEA //Driver timing control B 
+#define PWONSCTL	0xED //Power on sequence control 
+#define EN3G		0xF2 //Enable_3G 
+#define PRCTL		0xF7 //Pump ratio control 
 
 static inline void WR_CMD(uint16_t cmd)
 {
@@ -42,7 +136,20 @@ static uint16_t read_reg(uint16_t addr)
 
 uint16_t ili9320_get_id(void)
 {
-    return read_reg(0x00);
+    uint32_t id;
+    id = read_reg(0x00);
+    
+    /* try ILI9341 */
+    if(id < 0XFF || id == 0XFFFF || id == 0X9300)
+    {
+        WR_CMD(0XD3);			   
+		id = RD_DATA();//dummy read 	
+ 		id = RD_DATA();
+  		id = RD_DATA();   								   
+ 		id <<= 8;
+		id |= RD_DATA();   	
+    }
+    return id;
 }
 
 static uint16_t LCD_BGR2RGB(uint16_t c)
@@ -71,8 +178,8 @@ int ili9320_read_pixel(int x, int y)
 void ili9320_clear(int c)
 {
 	int i;
-    ili9320_set_window(0, 0, LCD_X_MAX, LCD_Y_MAX);
-	WR_CMD(0x22);
+    ili9320_set_window(0, 0, 50, 100);
+    WR_CMD(lcd_dev.cmd_gram);
 	for(i = 0; i < (LCD_X_MAX * LCD_Y_MAX); i++)
 	{
 		WR_DATA(c);	   
@@ -87,9 +194,10 @@ void ili9320_write_gram(uint16_t *buf, int len)
         WR_DATA(*buf++);
     }
 }
+
 void ili9320_write_pixel(int x, int y, int c)
 {
-    switch(lcd_id)
+    switch(lcd_dev.id)
     {
         case 0x9320:
             write_reg(0x0020, x);
@@ -111,9 +219,20 @@ void ili9320_write_pixel(int x, int y, int c)
 
 void ili9320_set_window(int x, int y, int xlen, int ylen)
 {
-
-    switch(lcd_id)
+    switch(lcd_dev.id)
     {
+        case 0x9341:
+            WR_CMD(lcd_dev.cmd_setx); 
+            WR_DATA(x>>8); 
+            WR_DATA(x&0XFF);	 
+            WR_DATA(xlen>>8); 
+            WR_DATA(xlen&0XFF);  
+            WR_CMD(lcd_dev.cmd_sety); 
+            WR_DATA(y>>8); 
+            WR_DATA(y&0XFF); 
+            WR_DATA(ylen>>8); 
+            WR_DATA(ylen&0XFF); 
+            break;
         case 0x9320:
             write_reg(0x0020, x);
             write_reg(0x0021, y);
@@ -136,7 +255,7 @@ void ili9320_set_window(int x, int y, int xlen, int ylen)
 
 void ili9320_hline(int xs, int xe, int y, int c)
 {
-    switch(lcd_id)
+    switch(lcd_dev.id)
     {
         case 0x9320:
             write_reg(0x03,(1<<5)|(1<<4)|(0<<3)|(1<<12));
@@ -172,7 +291,7 @@ void ili9320_hline(int xs, int xe, int y, int c)
 void ili9320_vline(int ys, int ye, int x, int c)
 {
     
-    switch(lcd_id)
+    switch(lcd_dev.id)
     {
         case 0x9320:
             write_reg(0x03,(0<<5)|(0<<4)|(1<<3)|(1<<12));
@@ -293,16 +412,119 @@ int ili9320_init(void)
     DelayMs(1);
     GPIO_WriteBit(gpio_instance, 19, 1);
     DelayMs(1);
-    lcd_id = ili9320_get_id();
-    switch(lcd_id)
+    
+    lcd_dev.id = ili9320_get_id();
+    switch(lcd_dev.id)
     {
+        case 0x9341:
+            lcd_dev.cmd_setx = 0x2A;
+            lcd_dev.cmd_sety = 0x2B;  
+            lcd_dev.cmd_gram = 0x2C;  
+            
+            WR_CMD(PWCTLB);
+            WR_DATA(0x00);
+            WR_DATA(0x81);
+            WR_DATA(0x30);
+            WR_CMD(PWONSCTL);
+            WR_DATA(0x64);
+            WR_DATA(0x03);
+            WR_DATA(0x12);
+            WR_DATA(0x81);
+            WR_CMD(DTIMCTLA);
+            WR_DATA(0x85);
+            WR_DATA(0x10);
+            WR_DATA(0x78);
+            WR_CMD(PWCTLA);
+            WR_DATA(0x39);
+            WR_DATA(0x2C);
+            WR_DATA(0x00);
+            WR_DATA(0x34);
+            WR_DATA(0x02);
+            WR_CMD(PRCTL);
+            WR_DATA(0x20);
+            WR_CMD(DTIMCTLB);
+            WR_DATA(0x00);
+            WR_DATA(0x00);
+            WR_CMD(FRMCTR1);
+            WR_DATA(0x00);
+            WR_DATA(0x1B);
+            WR_CMD(DISCTRL);
+            WR_DATA(0x0A);
+            WR_DATA(0xA2);
+            WR_CMD(PWCTRL1);
+            WR_DATA(0x35);
+            WR_CMD(PWCTRL2);
+            WR_DATA(0x11);
+            WR_CMD(VMCTRL1);
+            WR_DATA(0x45);
+            WR_DATA(0x45);
+            WR_CMD(VMCTRL2);
+            WR_DATA(0xA2);
+            WR_CMD(EN3G);
+            WR_DATA(0x00);
+            WR_CMD(GAMSET);
+            WR_DATA(0x01);
+            WR_CMD(PGAMCTRL); 
+            WR_DATA(0x0F);
+            WR_DATA(0x26);
+            WR_DATA(0x24);
+            WR_DATA(0x0B);
+            WR_DATA(0x0E);
+            WR_DATA(0x09);
+            WR_DATA(0x54);
+            WR_DATA(0xA8);
+            WR_DATA(0x46);
+            WR_DATA(0x0C);
+            WR_DATA(0x17);
+            WR_DATA(0x09);
+            WR_DATA(0x0F);
+            WR_DATA(0x07);
+            WR_DATA(0x00);
+            WR_CMD(NGAMCTRL);
+            WR_DATA(0x00);
+            WR_DATA(0x19);
+            WR_DATA(0x1B);
+            WR_DATA(0x04);
+            WR_DATA(0x10);
+            WR_DATA(0x07);
+            WR_DATA(0x2A);
+            WR_DATA(0x47);
+            WR_DATA(0x39);
+            WR_DATA(0x03);
+            WR_DATA(0x06);
+            WR_DATA(0x06);
+            WR_DATA(0x30);
+            WR_DATA(0x38);
+            WR_DATA(0x0F);
+            WR_CMD(MADCTL);
+            WR_DATA(0x08);
+            WR_CMD(CASET); 
+            WR_DATA(0x00);
+            WR_DATA(0x00);
+            WR_DATA(0x00);
+            WR_DATA(0xEF);
+            WR_CMD(PASET); 
+            WR_DATA(0x00);
+            WR_DATA(0x00);
+            WR_DATA(0x01);
+            WR_DATA(0x3F);
+            WR_CMD(PIXSET); 
+            WR_DATA(0x05);
+            WR_CMD(SLPOUT);
+            DelayMs(20);
+            WR_CMD(DISPON);
+            break;
         case 0x9320:
+            lcd_dev.cmd_setx = 0x20;
+            lcd_dev.cmd_sety = 0x21;  
+            lcd_dev.cmd_gram = 0x22;  
+          
             write_reg(0xe5,0x8000);  // Set the internal vcore voltage    
             write_reg(0x00,0x0001);  // start OSC    
             write_reg(0x2b,0x0010);  //Set the frame rate as 80 when the internal resistor is used for oscillator circuit    
             write_reg(0x01,0x0100);  //s720  to  s1 ; G1 to G320    
             write_reg(0x02,0x0700);  //set the line inversion    
-            //LCD_WR_REG(0x03,0x1018);  //65536 colors     
+            //WR_CMD(0x03,0x1018);  //65536 colors     
             write_reg(0x03,0x1030);   
             //??
             #ifdef LCD_USE_HORIZONTAL
@@ -368,6 +590,10 @@ int ili9320_init(void)
             ret = 0;
             break;
         case 0x8989:
+            lcd_dev.cmd_setx =0x4e;
+            lcd_dev.cmd_sety =0x4f;  
+            lcd_dev.cmd_gram =0x22;
+          
             write_reg(0x0000,0x0001);
             write_reg(0x0003,0xA8A4);
             write_reg(0x000C,0x0000);     
@@ -422,18 +648,23 @@ int ili9320_init(void)
 
 void GUI_DrawPixel(int color, int x, int y)
 {
-    switch(lcd_id)
+    switch(lcd_dev.id)
     {
         case 0x9320:
-            write_reg(0x0020, x);
-            write_reg(0x0021, y);
-            WR_CMD(0x0022);
+        case 0x8989: 
+            write_reg(lcd_dev.cmd_setx, x);
+            write_reg(lcd_dev.cmd_sety, y);
+            WR_CMD(lcd_dev.cmd_gram);
             WR_DATA(color);
             break;
-        case 0x8989:
-            write_reg(0x004e,x);        
-            write_reg(0x004f,y);  
-            WR_CMD(0x0022);
+        case 0x9341:
+            WR_CMD(lcd_dev.cmd_setx); 
+            WR_DATA(x>>8); 
+            WR_DATA(x&0XFF);	 
+            WR_CMD(lcd_dev.cmd_sety); 
+            WR_DATA(y>>8); 
+            WR_DATA(y&0XFF);
+            WR_CMD(lcd_dev.cmd_gram); 
             WR_DATA(color);
             break;
         default:

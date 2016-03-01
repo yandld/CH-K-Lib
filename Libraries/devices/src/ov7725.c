9bd7cae1b4816480f0088a775eee800428073240
Yandld <yandld@126.com>
2016-03-01 21:20:33 +0800

    modify OV7725 reg value

--- Libraries/devices/src/ov7725.c
+++ Libraries/devices/src/ov7725.c
@@ -180,8 +180,8 @@ static uint8_t ov7725_addr[] = {0x21};
 static const struct ov7725_reg reg_tbl[] =
 {
 
-    {OV7725_COM4         , 0x41},
-    {OV7725_CLKRC        , 0x02},
+    {OV7725_COM4         , 0XC1},
+    {OV7725_CLKRC        , 0X02},
     {OV7725_COM2         , 0x03},
     {OV7725_COM3         , 0xD0},
     {OV7725_COM7         , 0x40},
